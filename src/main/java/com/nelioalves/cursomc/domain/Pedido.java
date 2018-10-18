6161eaa522020349cab2ab59013d8193834f9962
thiagoneriramos <thiagoneriramos@yahoo.com.br>
2018-10-18 13:51:51 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -1,7 +1,11 @@
 package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -33,7 +38,8 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
-	
+   @OneToMany(mappedBy="id.pedido")	
+   private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -109,7 +115,17 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
 
+
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
+	
 
 	@Override
 	public int hashCode() {
@@ -137,8 +153,10 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
+
+
+
 	
 	
 	
